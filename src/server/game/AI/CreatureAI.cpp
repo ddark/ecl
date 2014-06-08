aaf16f7dcc73e36d80cd7079e8a88476f5877ecd
ddark93 <dark-lord93@yandex.ru>
2014-06-08 20:19:54 +0400

    1

--- src/server/game/AI/CreatureAI.cpp
+++ src/server/game/AI/CreatureAI.cpp
@@ -170,8 +170,6 @@ void CreatureAI::EnterEvadeMode()
 
     if (me->IsVehicle()) // use the same sequence of addtoworld, aireset may remove all summons!
         me->GetVehicleKit()->Reset(true);
-
-    me->SetLastDamagedTime(0);
 }
 
 /*void CreatureAI::AttackedBy(Unit* attacker)
@@ -241,6 +239,7 @@ bool CreatureAI::_EnterEvadeMode()
     me->LoadCreaturesAddon();
     me->SetLootRecipient(NULL);
     me->ResetPlayerDamageReq();
+    me->SetLastDamagedTime(0);
 
     if (me->IsInEvadeMode())
         return false;
